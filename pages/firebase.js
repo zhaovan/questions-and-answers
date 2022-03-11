aac51588e3101c4b2efcffcf1c4da5a6c115bca8
Ivan Zhao <ivanzhao8@gmail.com>
2022-03-11 17:13:03 -0500

    initial push for q&a project

---
+++ pages/firebase.js
@@ -0,0 +1,11 @@
+export default firebaseConfig = {
+  apiKey: "AIzaSyB7akJhJJlEnZjVyXe6Jykh4fxDFSVv38E",
+  authDomain: "questions-and-answers-14125.firebaseapp.com",
+  databaseURL:
+    "https://questions-and-answers-14125-default-rtdb.firebaseio.com",
+  projectId: "questions-and-answers-14125",
+  storageBucket: "questions-and-answers-14125.appspot.com",
+  messagingSenderId: "518960628693",
+  appId: "1:518960628693:web:4d9d39cf90613eba75f35c",
+  measurementId: "G-RYD3CQVPHM",
+};
